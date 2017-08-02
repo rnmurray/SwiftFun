7b2019916ff3565b691aab06a70a944d12d3e8c5
Robyn Murray <rnmurray@ucdavis.edu>
2017-08-02 15:26:11 -0700

    First commit after button tutorial!

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var buttonCount = 0
+    
+    //add label outlet
+    @IBOutlet weak var myLabel: UILabel!
+    
+    //add button action
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        buttonCount += 1
+        print(buttonCount)
+        
+        if buttonCount >= 10 {
+            view.backgroundColor = UIColor.cyan
+            myLabel.textColor = UIColor.blue
+            myLabel.text = "Coding is Fun!"
+        }
+        
+        if buttonCount >= 15 {
+            view.backgroundColor = UIColor.black
+            myLabel.textColor = UIColor.white
+            myLabel.text = "Stop Pressing ME!"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLabel.textColor = UIColor.green
+        myLabel.text = "Robyn is Awesome"
     }
 
     override func didReceiveMemoryWarning() {
